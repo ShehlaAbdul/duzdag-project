2b0fd37ee42dd8228991f05247e6d3cf632ebe9a
shehlaabdullayeva2004 <mi7ttwzlc@code.edu.az>
2025-11-12 12:54:26 +0400

    navbar is changed

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -4,6 +4,7 @@ import Mail from "../../assets/icons/mail.svg";
 import Phone from "../../assets/icons/phone.svg";
 import Logo from "../../assets/images/logo-blue.webp";
 import LogoWhite from "../../assets/images/logo_white.webp";
+import Logo2 from "../../assets/images/logo-pasha.webp"
 import { Link } from "react-router-dom";
 import { IoIosArrowDown } from "react-icons/io";
 import { LiaTimesSolid } from "react-icons/lia";
@@ -46,9 +47,14 @@ function Header() {
         </div>
       </div>
 
-      <div className="custom-navbar navbar-expand-lg px-3  px-md-5 px-lg-1  d-flex align-items-center justify-content-between border">
-        <div className="logo col-auto">
-          <Link to={"/"}>
+      <div className="custom-navbar navbar-expand-lg px-3  px-md-5 px-lg-1  d-flex align-items-center justify-content-between ">
+        <div className="logo-side col-auto d-flex gap-2">
+          <div className="logo">
+            <Link to={"/"}>
+              <img src={Logo2} alt="" />
+            </Link>
+          </div>
+          <Link to={"/"} className="logo">
             <img src={scroll ? LogoWhite : Logo} alt="Logo" />
           </Link>
         </div>
@@ -91,24 +97,23 @@ function Header() {
           </ul>
         </div>
         <div className="d-flex justify-content-end align-items-center gap-0">
-      
-         
           <div className="d-block d-lg-none  " onClick={toggleSidebar}>
-            <HiBars3 size={20} className="text-dark" />
+            <HiBars3 size={20} className="bar" />
           </div>
         </div>
       </div>
       <div className={`sidebar ${isOpen ? "open" : ""}`}>
         <div className="sidebar-header d-flex justify-content-between align-items-center px-3 py-3 border-bottom">
-          <div className="d-flex align-items-center justify-content-between w-100">
+          <div className="d-flex align-items-center justify-content-between w-100 ">
             <Link to="/" className="navbar-brand">
               <img src={Logo} alt="Logo" className="nav-logo" />
             </Link>
-            <LiaTimesSolid size={20} onClick={toggleSidebar} />
+            <div className=" ">
+              <LiaTimesSolid size={20} onClick={toggleSidebar} />
+            </div>
           </div>
         </div>
         <div className="sidebar-body d-flex flex-column align-items-center  py-4 gap-3">
-          
           <div className="w-100 align-items-start  ">
             <Link to="/" className="nav-link">
               Ana Səhifə
